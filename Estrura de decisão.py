9714ab66e18c2f18907fde519cbf778c7b7e78bf
PabloG2022 <pabloguilherme90@hotmail.com>
2022-09-26 11:36:26 -0300

    Estrutura de decisão

--- Estrura de decisão.py
+++ Estrura de decisão.py
@@ -1,3 +1,7 @@
+#Faça um Programa que verifique se uma letra digitada
+# é "F" ou "M". Conforme a letra escrever:
+# F - Feminino, M - Masculino, Sexo Inválido.
+
 Letra = input("digite o sexo M/F: ")
 if Letra == 'M'or Letra == 'm':
   print('sexo masculino')
