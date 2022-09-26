b6285700be034d125e0cc9d28cd4062490e0d8ea
PabloG2022 <pabloguilherme90@hotmail.com>
2022-09-26 11:31:46 -0300

    Estrutura de decisão

---
+++ Estrura de decisão.py
@@ -0,0 +1,7 @@
+Letra = input("digite o sexo M/F: ")
+if Letra == 'M'or Letra == 'm':
+  print('sexo masculino')
+elif Letra == 'F' or Letra == 'f':
+  print('sexo feminino')
+else:
+  print('sexo inválido')
