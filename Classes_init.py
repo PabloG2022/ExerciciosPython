79cef8894e9bd4912acb310c4b794f30dfe025b1
PabloG2022 <pabloguilherme90@hotmail.com>
2022-09-29 11:11:13 -0300

    Criando Classes utilizando __init__

---
+++ Classes_init.py
@@ -0,0 +1,14 @@
+#Classe Quadrado: Crie uma classe que modele um quadrado:
+
+#Atributos: Tamanho do lado
+#Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
+
+class Quadrado:
+  def __init__(self, lado = 1):
+    self.lado = lado
+
+  def calcular_area(self):
+    return self.lado **2
+
+quadrado =Quadrado(4)
+print(quadrado.lado, quadrado.calcular_area())
